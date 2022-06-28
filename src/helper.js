c40d50d7490f62f21cec720a5641feb67cab1150
Thiago Lagden <lagden@gmail.com>
2022-06-28 06:12:57 -0300

    fix intersection

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,3 @@
-const _toArray = v => Array.isArray(v) ? v : [v]
-
 /**
  * Helper intersection
  * @param {Array<string, number, boolean>} v1 - Array com valores primitivos
@@ -7,6 +5,7 @@ const _toArray = v => Array.isArray(v) ? v : [v]
  * @return {boolean} Retorna verdadeiro se existir o mesmo elemento no v1 e v2
  */
 export function intersection(v1, v2) {
+	const _toArray = v => Array.isArray(v) ? v : [v]
 	const a = new Set(_toArray(v1))
 	const b = new Set(_toArray(v2))
 	const _intersection = new Set([...a].filter(x => b.has(x)))
