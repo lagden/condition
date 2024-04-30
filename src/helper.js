7dbea9b4a6cbf0705d6ae8f277e395a2f2ad3e27
Thiago Lagden <lagden@gmail.com>
2024-04-30 02:19:16 -0300

    add tadashi as dep

--- src/helper.js
+++ src/helper.js
@@ -156,31 +156,4 @@ export function length(v, size, compare) {
 	return m(String(v).length, size)
 }
 
-/**
- * Retrieves a value from a nested object based on a given path.
- *
- * @param {Object} obj - The object from which to retrieve the value.
- * @param {string} path - The path to the desired value, using dot notation (e.g., 'parent.child.grandchild').
- * @returns {*} - The value found at the specified path, or undefined if not found.
- */
-export function getValueFromObject(obj, path) {
-	// Split the path string into an array of keys.
-	const keys = path.split('.')
-
-	// Initialize the value to the root object.
-	let value = obj
-
-	// Iterate through each key in the path array.
-	for (const key of keys) {
-		// Access the value corresponding to the current key.
-		value = value[key]
-
-		// If the value is undefined, return undefined.
-		if (value === undefined) {
-			return undefined
-		}
-	}
-
-	// Return the final value found at the end of the path.
-	return value
-}
+export {getValueFromObject} from '@tadashi/common/src/lib/get-value-from-object.js'
